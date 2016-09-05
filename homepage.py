6abc0f05f3087bade7cb4ee81dab3876895cd656
Andrew Brookins <abrookins@oreilly.com>
2016-09-05 16:19:48 -0700

    Add docstring for HomepageResource

--- homepage.py
+++ homepage.py
@@ -14,6 +14,26 @@ popular = ApiClient('popular')
 
 
 class HomepageResource:
+    """A resource that returns data for a user's homepage.
+
+    GET from this endpoint to retrieve homepage data:
+
+        $ curl -v -H "Authorization: Token 0x132" "http://192.168.99.100:8001/home"
+
+    Response:
+
+        < HTTP/1.1 200 OK
+        < Server: gunicorn/19.6.0
+        < Date: Mon, 05 Sep 2016 23:19:05 GMT
+        < Connection: close
+        < content-type: application/json; charset=UTF-8
+        < content-length: 119
+        <
+        * Closing connection 0
+        {"recommendations": [12, 23, 100, 122, 220, 333, 340, 400, 555, 654], "popular_items": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+
+    Note that the request must include an authentication token.
+    """
     def on_get(self, req, resp):
         """Return data for the homepage."""
         auth_header = req.context.get('auth_header')
